b4a293487858597718ef9116e0ad96a7a04a80f2
Akhil Nair <4khiln4ir@gmail.com>
2020-10-07 03:35:15 +0530

    feat: completed project

--- src/Components/ImageCardContainer/ImageCardContainer.jsx
+++ src/Components/ImageCardContainer/ImageCardContainer.jsx
@@ -3,24 +3,111 @@
  */
 
 // Import Modules
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { motion, useCycle, AnimatePresence } from 'framer-motion';
+import { motion, useCycle } from 'framer-motion';
 
 // Import Styles
 import style from './ImageCardContainer.module.css';
 
+// Import Media
+import BackArrowIcon from '../../Media/svg/left-arrow.svg';
+import MoneyBagIcon from '../../Media/svg/money-bag.svg';
+import MoneyWingIcon from '../../Media/svg/money-wings.svg';
+import FireIcon from '../../Media/svg/fire.svg';
+
 const ImageCardContainer = ({ cardDetails }) => {
-  const [animate, cycleCard] = useCycle();
-  console.log('Image Card Container');
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [animate, expand] = useCycle(
+    {
+      card: { position: 'relative', height: 'auto' },
+      backButton: { height: '0px', cursor: 'pointer', visibility: 'hidden' },
+      content: { height: '0px', padding: '0px' },
+    },
+    {
+      card: {
+        height: '100vh', width: '320px', top: '0px', position: 'absolute', cursor: 'default',
+      },
+      backButton: { height: '60px', visibility: 'visible' },
+      content: { height: '520px', cursor: 'default', padding: '5px' },
+    },
+  );
+
+  const toggleExpand = (shouldExpand) => {
+    if (shouldExpand) {
+      setIsExpanded((prevState) => !prevState);
+      expand();
+    }
+  };
+
   return (
     <div className={style.imageCardContainer}>
       {cardDetails.cards.map((card) => (
-        <motion.div
-          key={card.name}
-          className={style.imageCard}
-          style={{ backgroundImage: `url(${card.bg_image.image_url})` }}
-        />
+        <motion.div animate={animate.card} key={card.name} className={style.imageCard}>
+          <motion.div className={style.hiddenBackButton} animate={animate.backButton} onTap={() => toggleExpand(isExpanded)}>
+            <img src={BackArrowIcon} alt="Go back" />
+            Savings Challenge
+          </motion.div>
+          <motion.div className={style.cardImage} style={{ backgroundImage: `url(${card.bg_image.image_url})` }} alt="Card" onTap={() => toggleExpand(!isExpanded)} />
+          <motion.div className={style.hiddenContent} animate={animate.content}>
+            <div className={style.firstRow}>
+              <div className={style.firstRowCard}>
+                <div className={style.firstRowCardTitle}>
+                  ₹10
+                  <img src={MoneyBagIcon} alt="MoneyBag" />
+                </div>
+                <br />
+                Min day savings
+              </div>
+              <div className={style.firstRowCard}>
+                <div className={style.firstRowCardTitle}>
+                  10 days
+                  <img src={FireIcon} alt="Fire" />
+                </div>
+                <br />
+                Streak to maintain
+              </div>
+            </div>
+            <div className={style.secondRow}>
+              <div className={style.secondRowTitle}>
+                Upto ₹2000
+                <img src={MoneyWingIcon} alt="Win Money" />
+              </div>
+              Instant rewards for successful 10-Day streak
+            </div>
+            <div className={style.rulesTitle}>Remember the rules 🗒</div>
+            <div className={style.thirdRow}>
+              <ul>
+                <li>
+                  You must maintain a streak for 10 days
+                  <span role="img" aria-label="fire">🔥</span>
+                </li>
+                <li>
+                  To maintain the streak, you must add
+                  <span role="img" aria-label="money with wings">💸</span>
+                  to your savings once everyday
+                </li>
+                <li>
+                  Minimum savings to be added everyday must be ₹10 or above
+                  <span role="img" aria-label="money bag">💰</span>
+                </li>
+                <li>
+                  You can add a maximum of ₹200 everyday
+                  <span role="img" aria-label="hope">🤞🏻</span>
+                </li>
+                <li>
+                  The streak will break if you exit the challenge or
+                  do not add the minimum amount required
+                  <span role="img" aria-label="stunned">😵</span>
+                </li>
+                <li>
+                  You lose if you break the savings streak
+                  <span role="img" aria-label="sad">😔</span>
+                </li>
+              </ul>
+            </div>
+          </motion.div>
+        </motion.div>
       ))}
     </div>
   );
